2f51c3b0505510e643cab76c04f2f337b9c9e156
Medox36 <giuntinil@bzz.ch>
2022-09-02 22:51:12 +0200

    implemented the TranslationFileReader class for reading the translations.txt file

---
+++ src/main/java/ch/giuntini/languageclilearntool/FileCreateException.java
@@ -0,0 +1,8 @@
+package ch.giuntini.languageclilearntool;
+
+public class FileCreateException extends RuntimeException {
+
+    public FileCreateException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ch/giuntini/languageclilearntool/TranslationFileReader.java
@@ -0,0 +1,40 @@
+package ch.giuntini.languageclilearntool;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TranslationFileReader {
+
+    private final File file;
+
+    public TranslationFileReader() {
+        file = new File("translations.txt");
+    }
+
+    public List<Sentence> readSentences() throws IOException {
+        if (!file.exists()) {
+            if (file.createNewFile()) {
+                Files.write(file.toPath(), StandardSentences.TEXT.getBytes(StandardCharsets.UTF_8));
+            } else {
+                throw new FileCreateException("The file for saving the translations couldn't be created");
+            }
+        }
+        List<String> stringList = Files.readAllLines(file.toPath());
+        List<Sentence> sentences = new ArrayList<>();
+        for (String str : stringList) {
+            // ignore comments
+            if (str.startsWith("##")) {
+                continue;
+            }
+            String[] temp = str.split(";");
+            sentences.add(new Sentence(temp[0], temp[1]));
+        }
+
+        return sentences;
+    }
+
+}
