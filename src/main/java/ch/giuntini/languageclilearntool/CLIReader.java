dc24ecbe9fa6456fd2658e8b7bfb35d83988626a
Medox36 <giuntinil@bzz.ch>
2022-09-03 16:15:48 +0200

    implemented method to read an int option as user input form a given array

--- src/main/java/ch/giuntini/languageclilearntool/CLIReader.java
+++ src/main/java/ch/giuntini/languageclilearntool/CLIReader.java
@@ -1,5 +1,6 @@
 package ch.giuntini.languageclilearntool;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CLIReader implements AutoCloseable {
@@ -11,6 +12,32 @@ public class CLIReader implements AutoCloseable {
 
     public String readLine() {
         return scanner.nextLine();
+    }
+
+    public int readIntOption(String text, int... options) {
+        int chosenOption;
+        boolean inArray;
+        do {
+            chosenOption = readInt(text);
+            int finalChosenOption = chosenOption;
+            inArray = Arrays.stream(options).anyMatch(value -> value == finalChosenOption);
+        } while (!inArray);
+
+        return chosenOption;
+    }
+
+    public int readInt(String text) {
+        System.out.println(text);
+        System.out.print("  >");
+        while (!scanner.hasNextInt()) {
+            System.out.println(text);
+            System.out.print("  >");
+            scanner.next();
+        }
+        int chosenOption = scanner.nextInt();
+        scanner.nextLine();
+
+        return chosenOption;
     }
 
     @Override
