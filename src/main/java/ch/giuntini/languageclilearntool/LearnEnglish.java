52700ed009e412a5522971e5249be92b20c686b0
Medox36 <giuntinil@bzz.ch>
2022-09-08 22:56:06 +0200

    added the two learning classes

---
+++ src/main/java/ch/giuntini/languageclilearntool/LearnEnglish.java
@@ -0,0 +1,15 @@
+package ch.giuntini.languageclilearntool;
+
+public class LearnEnglish {
+
+    private final LearnUtil util;
+
+    public LearnEnglish(LearnUtil util) {
+        this.util = util;
+    }
+
+    public void startLearning() {
+
+    }
+
+}
---
+++ src/main/java/ch/giuntini/languageclilearntool/LearnGerman.java
@@ -0,0 +1,14 @@
+package ch.giuntini.languageclilearntool;
+
+public class LearnGerman {
+
+    private final LearnUtil util;
+
+    public LearnGerman(LearnUtil util) {
+        this.util = util;
+    }
+
+    public void startLearning() {
+
+    }
+}
